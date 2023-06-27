3643bb624266c4aaa03841b2cf026244082f441e
Rebekah <rebekah@Rebekahs-MacBook-Pro.local>
2023-06-27 14:39:02 -0400

    file created, tasks listed

---
+++ gameofwar.js
@@ -0,0 +1,27 @@
+// to submit this when finished, post an issue on the GameOfWar repository
+// with a link to your repository on GitHub(not GitHub Enterprise)
+
+// Tasks: 
+// - Your game should run without errors
+// - The game starts immediately when the JavaScript is executed. 
+// - Create your two players and start the rounds!
+// - There is no user input. The program simply loops through rounds until the game is finished.
+// - Print a message for each round showing the cards played by each user,
+//   who won the round, and how many cards each player has.
+// - Include a README written in well-formatted Markdown (hint: look up README templates)
+// - Show a good commit history with frequent commits (We're looking for lots of small commits!)
+
+// Hints
+// - Adding a property for the "value" of a rank could make it easier to compare face cards
+//   (ie: Jack < Queen).For example, 2 - 10 will represent each number 
+//   and then 11, 12, 13, 14 for J Q K A, respectively.
+// - It could be helpful to have a class to represent the overall Game,
+//   particularly when handling ties and other complicated game states.
+// - Write Pseudocode! Break the project down into different components. 
+//   What are the pieces to this problem ? Solve them each individually and build 
+//   towards a solution for the overall problem.Teach each piece individually before moving on!
+// - Make it work, make it good, make it fast.This is a common developer axiom. 
+//   It means: focus first on making something work
+//   (even if it isn't well written or it's buggy).
+//   Then, turn to making it work well.Then, and only then, start to worry about 
+//   other things like performance.
